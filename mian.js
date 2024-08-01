0efa4c5e76be95d5fa170ea647094461a598fcbe
zohidbe <zohaxaydarov@gmail.com>
2024-08-01 23:45:10 +0500

    end homwork

---
+++ mian.js
@@ -0,0 +1,20 @@
+// 1- chi misol 
+// let number = 12345;
+
+// let minglikQism = Math.floor(number / 1000);
+
+
+// let minglikRaqam = minglikQism % 10;
+
+// console.log(`Kirish: ${number}, Minglik xonasidagi raqam: ${minglikRaqam}`);
+
+
+// Uch xonali son
+// let number = 121233;
+// let birlikRaqam = number % 10;
+
+// let qolganRaqamlar = Math.floor(number / 10);
+
+// let yangiSon = parseInt(birlikRaqam.toString() + qolganRaqamlar.toString());
+
+// console.log(`Kirish: ${number}, Yangi son: ${yangiSon}`);
